ec8e6c2788e3728bbdb79988aabf6daca1d9b2a3
T-a-n-d-E-m <the_tricky_trickster@protonmail.me>
2024-07-05 13:32:45 +1000

    shared constants will go in here eventually

---
+++ src/constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H_INCLUDED
+#define CONSTANTS_H_INCLUDED
+
+#include <stddef.h>
+
+// URLs can potentially be much longer than this but with Discord message character limits we want to keep things short and sweet.
+static const size_t URL_LENGTH_MAX = 512;
+
+#endif // CONSTANTS_H_INCLUDED
