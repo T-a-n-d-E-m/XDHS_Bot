8e8f70156f99860ea679a835c7dc108eb71247aa
T-a-n-d-E-m <the_tricky_trickster@protonmail.me>
2024-08-10 17:49:23 +1000

    Output different binaries for debug and release modes

--- build.sh
+++ build.sh
@@ -12,8 +12,10 @@
 
 if [[ $1 == 'release' ]]; then
 	BUILD_MODE="-DRELEASE -O3"
+	BINARY_NAME="xdhs_bot"
 else
 	BUILD_MODE="-DDEBUG -g -O3"
+	BINARY_NAME="xdhs_bot_dev"
 fi
 
 # Opts for Howard Hinnant's date/tz library.
@@ -24,10 +26,10 @@ LIBS="$(mariadb_config --include --libs) -ldpp -lfmt -lcurl -lpoppler-cpp -lpthr
 
 # -Wno-volatile for mongoose
 # -Wno-unused_function for stbi_resize
-time g++ -std=c++20 $BUILD_MODE -DXDHS_BOT -Wall -Werror -Wpedantic -Wno-volatile -Wno-unused-function -Wno-maybe-uninitialized -fno-rtti $LIB_DATE_OPTS -I./src/date ./src/tz.cpp ./src/xdhs_bot.cpp $LIBS -o xdhs_bot
+time g++ -std=c++20 $BUILD_MODE -DXDHS_BOT -Wall -Werror -Wpedantic -Wno-volatile -Wno-unused-function -Wno-maybe-uninitialized -fno-rtti $LIB_DATE_OPTS -I./src/date ./src/tz.cpp ./src/xdhs_bot.cpp $LIBS -o $BINARY_NAME
 
 # If compiling elsewhere...
 if [ "$HOSTNAME" != harvest-sigma ]; then
-	strip xdhs_bot
-	scp -o PubkeyAuthentication=no xdhs_bot tandem@harvest-sigma.bnr.la:~/dev/XDHS_Bot/
+	strip $BINARY_NAME
+	scp -o PubkeyAuthentication=no $BINARY_NAME tandem@harvest-sigma.bnr.la:~/dev/XDHS_Bot/
 fi
